b588ff92ef32c204dde11d17323afb96b201a151
Bina Abdul Rahim <binafshaii@icloud.com>
2021-08-15 22:25:02 -0400

    v2

--- password_generator.sh
+++ password_generator.sh
@@ -6,7 +6,7 @@ echo "Simple Password Generator"
 echo "Enter the length of password: "
 read PASS_LENGTH 
 
-for p in $(seq 1);
+for p in $(seq 1 5); #if you change seq 1 5 then you will get 5 generated password 
 do 
     #using openssl to generate a randomr character in base64 & cutting it form 1st character to inputed password length 
     openssl rand -base64 48 | cut  -c1-$PASS_LENGTH 
