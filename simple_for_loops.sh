fb5a77421b92020fa3859a6e46fac940aa5ca771
Bina Abdul Rahim <binafshaii@icloud.com>
2021-08-15 22:12:13 -0400

    nitial commiti

---
+++ simple_for_loops.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#SIMLE FOR LOOPS
+NAMES="JOR KEV ALI BRI"
+for NAME in $NAMES #Name - defining the variable and NAMES calling the variable above or calling the NAMES var
+    do
+        echo "HELLO $NAME" #using NAME variable 
+done
